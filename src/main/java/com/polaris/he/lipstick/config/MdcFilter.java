69c482485673dafaa8406a1fb48560c7333c8761
polaris <polaris1478@gmail.com>
2019-01-05 22:24:36 +0800

    mdc

---
+++ src/main/java/com/polaris/he/lipstick/config/MdcFilter.java
@@ -0,0 +1,37 @@
+package com.polaris.he.lipstick.config;
+
+import org.slf4j.MDC;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * User: hexie
+ * Date: 2019-01-05 22:02
+ * Description:
+ */
+@WebFilter(urlPatterns = "/*", filterName = "mdcFilter")
+public class MdcFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        try {
+            MDC.put("traceId", UUID.randomUUID().toString().replace("-", ""));
+            chain.doFilter(request, response);
+        } finally {
+            MDC.clear();
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
