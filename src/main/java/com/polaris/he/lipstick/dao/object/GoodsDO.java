6fd5090fc535c1e0241ed2791520d3374fa219df
polaris <polaris1478@gmail.com>
2019-01-07 23:09:31 +0800

    DO

--- src/main/java/com/polaris/he/lipstick/dao/object/GoodsDO.java
+++ src/main/java/com/polaris/he/lipstick/dao/object/GoodsDO.java
@@ -1,9 +1,28 @@
 package com.polaris.he.lipstick.dao.object;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  * User: hexie
  * Date: 2019-01-05 22:31
  * Description:
  */
-public class GoodsDO {
+@Getter
+@Setter
+@ToString
+public class GoodsDO extends BaseDO {
+
+    private Long id;
+
+    private String brandCode;
+
+    private String goodsCode;
+
+    private String goodsName;
+
+    private String illustration;
+
+    private String url;
 }
--- src/main/java/com/polaris/he/lipstick/dao/object/SkuDO.java
+++ src/main/java/com/polaris/he/lipstick/dao/object/SkuDO.java
@@ -1,9 +1,30 @@
 package com.polaris.he.lipstick.dao.object;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  * User: hexie
  * Date: 2019-01-05 22:32
  * Description:
  */
-public class SkuDO {
+@Getter
+@Setter
+@ToString
+public class SkuDO extends BaseDO {
+
+
+    private String brandCode;
+
+    private String goodsCode;
+
+    private String skuCode;
+
+    private String skuName;
+
+    private String url;
+
+    private JsonNode extension;
 }
